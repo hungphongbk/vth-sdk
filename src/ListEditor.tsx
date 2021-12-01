e1b1051e3183c4fa3803212ed9e4a33ca242381f
hungphongtruong <programmingd32@gmail.com>
2021-12-01 23:59:53 +0700

    update ListEditor.tsx

--- src/ListEditor.tsx
+++ src/ListEditor.tsx
@@ -1,17 +1,18 @@
 import { Stack } from "@mui/material";
 import { Control, useFieldArray } from "react-hook-form";
-import { ComponentType } from "react";
+import React, { ComponentType } from "react";
 import FormInput from "./FormInput";
 import { uniqueId } from "lodash";
-import React from "react";
 
-type ListEditorProps = {
+export type ListEditorOptions = { deletable?: boolean };
+export type ListEditorProps = {
   name: string;
   control: Control<any>;
   ItemComponent: ComponentType<any>;
   itemGenerator?: (index: number) => any;
   ListComponent?: ComponentType<any>;
   ListComponentProps?: any;
+  options?: { deletable?: boolean };
 };
 export default function ListEditor({
   name,
@@ -20,9 +21,11 @@ export default function ListEditor({
   itemGenerator = () => ({ id: uniqueId() }),
   ListComponent = Stack,
   ListComponentProps = {},
+  options: _options = {},
 }: ListEditorProps): JSX.Element {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { fields, append } = useFieldArray({ control, name });
+  const { fields, append, remove } = useFieldArray({ control, name });
+  const options: ListEditorOptions = { deletable: false, ..._options };
 
   const finishEditTemp = (e: any) => {
     append(e.target.value);
@@ -36,6 +39,7 @@ export default function ListEditor({
           key={field.id}
           control={control}
           component={ItemComponent}
+          onDelete={() => options.deletable && remove(index)}
         />
       ))}
       <ItemComponent
