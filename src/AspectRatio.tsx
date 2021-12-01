2b13557046ccc751d29ef5e890be7798f1e67a05
hungphongtruong <programmingd32@gmail.com>
2021-12-01 08:50:32 +0700

    add aspect ratio

---
+++ src/AspectRatio.tsx
@@ -0,0 +1,35 @@
+import { Box, styled } from "@mui/material";
+// import { AspectRatio as BaseAspectRatio } from 'react-aspect-ratio'
+
+export type AspectRatioProps = { ratio?: string | number };
+const AspectRatio = styled(Box)<AspectRatioProps>`
+  position: relative;
+  > :first-child {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+  }
+  > img {
+    height: auto;
+  }
+  ::before {
+    content: "";
+    display: block;
+    width: 100%;
+    @supports not (aspect-ratio: 1/1) {
+      height: 0;
+      padding-bottom: calc(100% / (${(props) => props.ratio}));
+    }
+    @supports (aspect-ratio: 1/1) {
+      aspect-ratio: calc(${(props) => props.ratio});
+    }
+  }
+`;
+
+AspectRatio.defaultProps = {
+  ratio: "1/1",
+};
+
+export default AspectRatio;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+export{default as AspectRatio}from './AspectRatio'
+export * from './AspectRatio'
 export{default as FormInput}from './FormInput'
 export{default as ListEditor}from './ListEditor'
