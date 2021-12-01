843f5b5ac1cdce8bdbb305fa1193a110de0d66d8
hungphongtruong <programmingd32@gmail.com>
2021-12-02 00:22:33 +0700

    add image uploader

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-export{default as AspectRatio}from './AspectRatio'
-export * from './AspectRatio'
+export { default as AspectRatio } from "./AspectRatio";
+export * from "./AspectRatio";
 
-export * as Constants from './constants';
+export * as Constants from "./constants";
 
-export{default as FormInput}from './FormInput'
-export{default as ListEditor}from './ListEditor'
+export { default as FormInput } from "./FormInput";
+
+export { default as ListEditor } from "./ListEditor";
+export * from "./ListEditor";
+
+export { default as ImageUploader } from "./ImageUploader";
+export * from "./ImageUploader";
