70f2c04255f6f2f2f413f2b44bf955e94e4afaf0
hungphongtruong <programmingd32@gmail.com>
2021-12-01 09:31:35 +0700

    add constants

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export enum SettingKeys {
+    Banner = 'common:banner'
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export{default as AspectRatio}from './AspectRatio'
 export * from './AspectRatio'
+
+export * as Constants from './constants';
+
 export{default as FormInput}from './FormInput'
 export{default as ListEditor}from './ListEditor'
