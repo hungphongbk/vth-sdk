e65b5eae24d3ef50cfe70cc9d3bc0dc39dfbf4e3
hungphongtruong <programmingd32@gmail.com>
2021-12-09 01:59:18 +0700

    add constant

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,26 @@ export enum SettingKeys {
   Banner = "common:banner",
   SiteInfo = "common:siteInfo",
 }
+
+export const SubmitInvestorConstants = {
+  purposes: [
+    "Xây dựng thương hiệu Việt",
+    "Mở rộng ngành hàng mới",
+    "Tìm kiếm lợi nhuận",
+    "Có thương hiệu riêng cho mình",
+    "Trở thành nhà đầu tư của Vaithuhay",
+  ],
+  methods: [
+    "Hợp tác phát triển sản phẩm",
+    "Hợp tác quảng cáo sản phẩm",
+    "Đầu tư dự án",
+  ],
+  funds: [
+    "Khoảng 10 triệu",
+    "Khoảng 50 triệu",
+    "Khoảng 100 triệu",
+    "Khoảng 500 triệu",
+    "Trên 1 tỷ",
+    "Trên 10 tỷ",
+  ],
+};
