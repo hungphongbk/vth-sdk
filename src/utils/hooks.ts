3bbef8ca41a48a91d6055f29a231d2dc64a9b052
hungphongtruong <programmingd32@gmail.com>
2021-12-26 07:14:07 +0700

    add use open close state

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -2,8 +2,8 @@ import { useCallback, useState } from "react";
 
 export const useOpenCloseState = () => {
   const [open, setOpen] = useState(false);
-  const doOpen = useCallback(() => setOpen(true), []),
-    doClose = useCallback(() => setOpen(false), []);
+  const doOpen: () => void = useCallback(() => setOpen(true), []),
+    doClose: () => void = useCallback(() => setOpen(false), []);
 
   return [open, doOpen, doClose];
 };
