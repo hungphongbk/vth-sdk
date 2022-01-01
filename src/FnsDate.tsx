67b900eb9160cefa709167bebac6182381d1b6f9
hungphongtruong <programmingd32@gmail.com>
2022-01-01 13:47:46 +0700

    add FnsDate

---
+++ src/FnsDate.tsx
@@ -0,0 +1,14 @@
+import { useMemo } from "react";
+import { format } from "date-fns";
+
+type FnsDateProps = {
+  value: string | Date;
+  format: string;
+};
+export default function FnsDate(props: FnsDateProps) {
+  const value = useMemo(() => {
+    if (typeof props.value === "string") return new Date(props.value);
+    return props.value;
+  }, [props.value]);
+  return format(value, props.format);
+}
