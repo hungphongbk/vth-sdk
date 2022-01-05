a8034a85c942c2a2729ebe7f99ae35392be45c9a
hungphongtruong <programmingd32@gmail.com>
2022-01-05 20:24:59 +0700

    follow mui guide

---
+++ src/themeAugmentation/index.d.ts
@@ -0,0 +1,3 @@
+export * from "./components";
+export * from "./overrides";
+export * from "./props";
--- src/themeAugmentation/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./components.d"
-export * from "./overrides.d"
-export * from "./props.d"
