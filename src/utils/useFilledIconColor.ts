cddcc44b1116e7fc9f86d0a978b0c077c956868b
hungphongtruong <programmingd32@gmail.com>
2022-01-16 20:38:49 +0700

    update icons

--- src/utils/useFilledIconColor.ts
+++ src/utils/useFilledIconColor.ts
@@ -4,7 +4,7 @@ import { useMemo } from "react";
 export function useFilledIconColor(color: SvgIconProps["color"]) {
   return useMemo(() => {
     if ((color as string) === "green") return ["#8affce", "#008F54"];
-    if ((color as string) === "gray") return ["#d5d5d5", "#ababab"];
+    if ((color as string) === "gray") return ["gray.light", "gray.dark"];
     return ["#fff5ca", "#ffcf00"];
   }, [color]);
 }
