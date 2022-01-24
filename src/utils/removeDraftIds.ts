fbcfed0bb4189858097fa10c3ea84f9410fa1834
hungphongtruong <programmingd32@gmail.com>
2022-01-25 02:08:54 +0700

    add util

--- src/utils/removeDraftIds.ts
+++ src/utils/removeDraftIds.ts
@@ -1,16 +1,17 @@
 import { reduce } from "lodash";
 
-export function removeDraftIds<T extends Record<string, any>>(original: T): T {
+export function removeDraftIds<T extends Record<string, any>>(original: T): T;
+export function removeDraftIds(original: any): any {
   if (typeof original !== "object") return original;
+  if (Array.isArray(original)) return original.map(removeDraftIds);
   return reduce(
     original,
     (acc, v, k) => {
       if (!(k === "id" && /^draft-/.test(v as string))) {
-        // @ts-ignore
         acc[k] = removeDraftIds(v);
       }
       return acc;
     },
-    {} as T
+    {} as any
   );
 }
