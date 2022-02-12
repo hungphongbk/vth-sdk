71a5bd731cf3892cb15875583546d19b2118be79
hungphongtruong <programmingd32@gmail.com>
2022-02-12 17:11:57 +0700

    try esm format

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import pkg from "./package.json";
 import typescript from "rollup-plugin-typescript2";
 import copy from "rollup-plugin-copy";
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 
 // Excluded dependencies
 const EXTERNAL = Object.keys(pkg.devDependencies);
@@ -23,6 +24,31 @@ export default {
     resolve(),
     typescript(),
     // commonjs(),
+    getBabelOutputPlugin({
+      presets: ["@babel/preset-env"],
+      plugins: [
+        [
+          "babel-plugin-direct-import",
+          {
+            modules: [
+              "@mui/lab",
+              "@mui/system",
+              "@mui/material",
+              "@mui/icons-material",
+            ],
+          },
+        ],
+        [
+          "babel-plugin-import",
+          {
+            libraryName: "lodash",
+            libraryDirectory: "",
+            camel2DashComponentName: false, // default: true
+          },
+        ],
+        "babel-plugin-date-fns",
+      ],
+    }),
     copy({
       targets: [
         { src: ".npmrc", dest: "dist" },
